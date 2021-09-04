530f0e0521b344b9579431a1c3475e6ccf5c19f6
Daniel Billotte <daniel@litfuze.com>
2021-09-04 13:35:45 -0700

    UnityMix works, needs UI love

--- src/UnityMix.cpp
+++ src/UnityMix.cpp
@@ -25,7 +25,43 @@ struct UnityMix : Module {
 		configParam(GAIN_PARAM, 0.f, 2.f, 1.f, "Output gain");
 	}
 
-	void process(const ProcessArgs& args) override {		
+	void process(const ProcessArgs& args) override {
+		// read all of the inputs and keep track
+		// of how many inputs there are
+		float in1=0.f, in2=0.f, in3=0.f;
+		int num_inputs = 0;
+		if(inputs[IN1_INPUT].isConnected()) {
+			in1 = inputs[IN1_INPUT].getVoltage();
+			num_inputs++;
+		}
+
+		if(inputs[IN2_INPUT].isConnected()) {
+			in2 = inputs[IN2_INPUT].getVoltage();
+			num_inputs++;
+		}
+
+		if(inputs[IN3_INPUT].isConnected()) {
+			in3 = inputs[IN3_INPUT].getVoltage();
+			num_inputs++;
+		}
+
+		// if no inputs, write out a 0 and return
+		if(num_inputs == 0) {
+			outputs[MIX_OUTPUT].setVoltage(0.f);
+			return;
+		}
+		
+		// calculate the input gain
+		float input_gain = 1.f/num_inputs;
+
+		// calculate the raw output sample
+		float output = (in1 + in2 + in3) * input_gain;
+
+		// read value of output gain knob
+		float output_gain = params[GAIN_PARAM].getValue();
+
+		// calculate and write out the output
+		outputs[MIX_OUTPUT].setVoltage(output * output_gain);
 	}
 };
 
